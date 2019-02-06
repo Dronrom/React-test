b5c242f538bd3d60bf9528c5dd7b20461ff1cd84
devdaroman <devdaroman@gmail.com>
2019-02-06 17:02:25 -0500

    .gitignore fix

---
+++ src/components/person-details/person-details.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+import SwapiService from '../../services/swapi-service';
+import './person-details.css';
+
+export default class PersonDetails extends Component {
+
+    swapiService = new SwapiService();
+  
+    state = {
+      person: null
+  };
+
+    componentDidMount(){
+        this.updatePerson();
+    }
+
+    componentDidUpdate(prevProps){
+        if (this.props.personId !== prevProps.personId){
+            this.updatePerson();
+        }
+    }
+
+    updatePerson() {
+        const { personId } = this.props;
+        if (!personId) {
+            return;
+        }
+
+        this.swapiService
+            .getPerson(personId)
+            .then((person) => {
+                this.setState({ person })
+            })
+  }
+
+  render() {
+
+    if (!this.state.person){
+        return <span>Select a person form a list </span>
+
+    }
+
+    const { person: {
+        id, name, gender, birthYear, eyeColor }} = this.state.person;
+
+    return (
+      <div className="person-details card">
+        <img className="person-image" 
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
+
+        <div className="card-body">
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Gender</span>
+              <span>{gender}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Birth Year</span>
+              <span>{birthYear}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Eye Color</span>
+              <span>{eyeColor}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
